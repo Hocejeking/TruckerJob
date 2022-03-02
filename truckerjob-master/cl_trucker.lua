12a738cb023a56c7715f0edfe0afb85f95ab5f82
Hocejeking <tobiashocevar@gmail.com>
2022-03-02 23:21:48 +0100

    Update 1.1
    Finished moving routes to config, made some changes to the used names of variables.

--- truckerjob-master/cl_trucker.lua
+++ truckerjob-master/cl_trucker.lua
@@ -8,22 +8,12 @@ local RouteSet = false
 local deliveryMarkerPos
 local HasAlreadyGotDeliveryMessage = false
 local alreadyDelivered = false
+local pickedroutes = {}
 
 
 
 
-local routes = {
-    vector3(30.1, 6285.4, 31.4),
-    vector3(303.2, 2882.8, 43.5),
-    vector3(183.8,2746.7,44.3),
-    vector3(538.2, -1978.1, 25),
-    vector3(1139.7,-1288.3,35.1),
-    vector3(1718.1,-1649.1,113),
-    vector3(2675.8, 1446.5, 25.4),
-    vector3(2702.4, 2864.6, 37.6),
-    vector3(1272.3, -3313.3, 6.1)
 
-}
 
 
 
@@ -32,6 +22,7 @@ Citizen.CreateThread(function()
 
     
     local ped = GetPlayerPed(-1)
+    InsertRoutes()
 
     while true do
 
@@ -77,7 +68,7 @@ Citizen.CreateThread(function()
 
         if TruckJobInProggres and not RouteSet then
             
-            deliveryblip = AddBlipForCoord(routes[math.random(#routes)])
+            deliveryblip = AddBlipForCoord(pickedroutes[math.random(#pickedroutes)])
             SetBlipSprite(deliveryblip, 304)
             SetBlipDisplay(deliveryblip, 4)
             SetBlipScale(deliveryblip, 1.0)
@@ -158,6 +149,16 @@ function VytvorTruck()
     
         exports[--[[Insert the LegacyFuel export here]]]:SetFuel(vehicle, 100) 
      
+
+    end
+
+end
+
+function InsertRoutes()
+
+    for k,v in ipairs(Config.routes) do
+            
+        table.insert(pickedroutes,v)
 
     end
 
--- truckerjob-master/config.lua
+++ truckerjob-master/config.lua
@@ -3,7 +3,20 @@ Config.payout = 250
 
 Config.useLegacyFuel = true -- If set to true, you need to set the legacyfuel export name in the client script
 
+Config.routes = 
+{
+    vector3(30.1, 6285.4, 31.4),
+    vector3(303.2, 2882.8, 43.5),
+    vector3(183.8,2746.7,44.3),
+    vector3(538.2, -1978.1, 25),
+    vector3(1139.7,-1288.3,35.1),
+    vector3(1718.1,-1649.1,113),
+    vector3(2675.8, 1446.5, 25.4),
+    vector3(2702.4, 2864.6, 37.6),
+    vector3(1272.3, -3313.3, 6.1)
+    --add your new routes here
 
+}
    
 
 
