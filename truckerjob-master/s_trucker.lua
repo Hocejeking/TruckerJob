eb46fe6e45dff1b5f834bb75f9e28a0ceae7427b
Hocejeking <53022454+Hocejeking@users.noreply.github.com>
2022-03-25 10:43:10 +0100

    Update 1.11
    General formatting of the server code.

--- truckerjob-master/s_trucker.lua
+++ truckerjob-master/s_trucker.lua
@@ -1,21 +1,12 @@
 ESX = nil
-
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
     end
 end)
-
-
-
-
-
-
-
 RegisterServerEvent('trucker:vyplata')
 AddEventHandler('trucker:vyplata', function()
-
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.addMoney(Config.payout)
     TriggerClientEvent('mystic:notify', source, 'Úspěsně si odevzdal zakázku a získal si '..Config.payout.. " $", 'succes')
